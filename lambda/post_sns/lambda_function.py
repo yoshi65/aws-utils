7a7419d00d0c8be2268607cd429f9e7a6ad546f7
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-08-01 11:34:28 +0900

    updated post_sns

--- lambda/post_sns/lambda_function.py
+++ lambda/post_sns/lambda_function.py
@@ -3,11 +3,10 @@
 #
 # FileName: 	lambda_function
 # CreatedDate:  2020-07-02 19:57:06 +0900
-# LastModified: 2020-07-17 17:32:19 +0900
+# LastModified: 2020-08-01 11:34:16 +0900
 #
 
 
-import json
 import boto3
 import os
 
@@ -15,7 +14,7 @@ import os
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
 
-    response = sns.Topic(os.environ['TOPIC_ARN']).publish(
+    sns.Topic(os.environ['TOPIC_ARN']).publish(
                 Message='post sns',
                 Subject='from lambda'
             )
