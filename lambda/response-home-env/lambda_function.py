8f108184af0d8639552bde23cf6b44fe09b134a2
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-08-01 11:33:43 +0900

    fixed for flake8

--- lambda/response-home-env/lambda_function.py
+++ lambda/response-home-env/lambda_function.py
@@ -3,7 +3,7 @@
 #
 # FileName: 	lambda_function
 # CreatedDate:  2020-07-02 19:57:06 +0900
-# LastModified: 2020-07-07 09:07:00 +0900
+# LastModified: 2020-08-01 11:28:30 +0900
 #
 
 
@@ -16,22 +16,22 @@ import os
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('inkbird')
-    d = sorted(table.scan()['Items'], key=lambda x:x['Date'], reverse=True)[0]
+    d = sorted(table.scan()['Items'], key=lambda x: x['Date'], reverse=True)[0]
     post_slack(d)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Success!')
     }
-    
-    
+
+
 def post_slack(d):
     SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
     payload = {
         'attachments': [{
             'color': '#D3D3D3',
             'pretext': f'Date: {d["Date"]}',
-            'text'   : f'Temperature: {d["Temperature"]}℃\nHumidity: {d["Humidity"]}%'
+            'text': f'Temperature: {d["Temperature"]}℃\nHumidity: {d["Humidity"]}%'
         }]
     }
 
