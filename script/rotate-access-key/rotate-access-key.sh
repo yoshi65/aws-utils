2409546754318d4a16156e62ce560adeff590c09
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-09-26 10:40:18 +0900

    updated rotate-access-key.sh

--- script/rotate-access-key/rotate-access-key.sh
+++ script/rotate-access-key/rotate-access-key.sh
@@ -1,13 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
 #
 # FileName: 	rotate-access-key
 # CreatedDate:  2020-08-27 12:50:19 +0900
-# LastModified: 2020-08-27 13:00:18 +0900
+# LastModified: 2020-09-26 10:39:48 +0900
 #
 
-current_access_key=$(aws configure get aws_access_key_id)
-username=$(aws iam get-user | jq -r '.User.UserName')
+set -ex
+
+if [ "$1" = "" ]; then
+  PROFILE="default"
+else
+  PROFILE=$1
+fi
+
+current_access_key=$(aws configure get aws_access_key_id --profile ${PROFILE})
+username=$(aws iam get-user --profile ${PROFILE} | jq -r '.User.UserName')
+echo "Create new access key for user ${username}"
 new_key=$(aws iam create-access-key --user-name $username)
-aws configure set aws_access_key_id $(jq -r '.AccessKey.AccessKeyId' <<< $new_key)
-aws configure set aws_secret_access_key $(jq -r '.AccessKey.SecretAccessKey' <<< $new_key)
+aws configure set aws_access_key_id $(jq -r '.AccessKey.AccessKeyId' <<< ${new_key}) --profile ${PROFILE}
+aws configure set aws_secret_access_key $(jq -r '.AccessKey.SecretAccessKey' <<< ${new_key}) --profile ${PROFILE}
 aws iam delete-access-key --access-key-id $current_access_key
