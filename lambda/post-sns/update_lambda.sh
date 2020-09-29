b3d814a9ebc7d73dfbc4c2aa7aebea40fa9f243a
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-09-29 21:12:17 +0900

    updated update_lambda.sh for post-sns

--- lambda/post-sns/update_lambda.sh
+++ lambda/post-sns/update_lambda.sh
@@ -5,4 +5,5 @@ set -eu
 rm -f function.zip
 zip -r9 function.zip lambda_function.py
 
-aws lambda update-function-code --function-name "post-sns" --zip-file fileb://function.zip --publish
+result=$(aws lambda update-function-code --function-name "post-sns" --zip-file fileb://function.zip --publish)
+echo "LastUpdateStatus: $(echo $result | jq -r '.LastUpdateStatus')"
