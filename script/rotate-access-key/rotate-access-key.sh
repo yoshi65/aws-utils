23a90f8f739f31283b84ddd12b5864bc219e0258
Yoshitaka Yamazaki <yoshitaka.yamazaki@bebit.com>
2021-01-25 14:39:30 +0900

    updated rotate-access-key to addd sleep

--- script/rotate-access-key/rotate-access-key.sh
+++ script/rotate-access-key/rotate-access-key.sh
@@ -2,7 +2,7 @@
 #
 # FileName: 	rotate-access-key
 # CreatedDate:  2020-08-27 12:50:19 +0900
-# LastModified: 2020-09-26 10:39:48 +0900
+# LastModified: 2021-01-25 14:39:08 +0900
 #
 
 set -ex
@@ -19,4 +19,4 @@ echo "Create new access key for user ${username}"
 new_key=$(aws iam create-access-key --user-name $username)
 aws configure set aws_access_key_id $(jq -r '.AccessKey.AccessKeyId' <<< ${new_key}) --profile ${PROFILE}
 aws configure set aws_secret_access_key $(jq -r '.AccessKey.SecretAccessKey' <<< ${new_key}) --profile ${PROFILE}
-aws iam delete-access-key --access-key-id $current_access_key
+sleep 5 && aws iam delete-access-key --access-key-id $current_access_key
