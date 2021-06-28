1d8a00638edc650674ddd7981cb05f2f32da7214
Yoshitaka Yamazaki <yoshitaka.yamazaki@bebit.com>
2021-06-28 09:07:07 +0900

    fixed rotate script

--- script/rotate-access-key/rotate-access-key.sh
+++ script/rotate-access-key/rotate-access-key.sh
@@ -2,7 +2,7 @@
 #
 # FileName: 	rotate-access-key
 # CreatedDate:  2020-08-27 12:50:19 +0900
-# LastModified: 2021-01-25 14:39:08 +0900
+# LastModified: 2021-06-28 09:06:47 +0900
 #
 
 set -ex
@@ -16,7 +16,7 @@ fi
 current_access_key=$(aws configure get aws_access_key_id --profile ${PROFILE})
 username=$(aws iam get-user --profile ${PROFILE} | jq -r '.User.UserName')
 echo "Create new access key for user ${username}"
-new_key=$(aws iam create-access-key --user-name $username)
+new_key=$(aws iam create-access-key --profile ${PROFILE} --user-name $username)
 aws configure set aws_access_key_id $(jq -r '.AccessKey.AccessKeyId' <<< ${new_key}) --profile ${PROFILE}
 aws configure set aws_secret_access_key $(jq -r '.AccessKey.SecretAccessKey' <<< ${new_key}) --profile ${PROFILE}
-sleep 5 && aws iam delete-access-key --access-key-id $current_access_key
+sleep 5 && aws iam delete-access-key --profile ${PROFILE} --access-key-id $current_access_key
