fe1eec4597f759d2804038c8d981efba4d46e3e9
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2022-12-18 14:31:52 +0900

    created check-keyball-stock

---
+++ lambda/check-keyball-stock/lambda_function.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json
+import logging
+import os
+
+from bs4 import BeautifulSoup
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    confirm_status()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "successfully sent to line",
+        }),
+    }
+
+
+def confirm_status():
+    logger.info("start confirm_status")
+
+    url = get_url()
+    site = requests.get(url)
+    data = BeautifulSoup(site.text, "html.parser")
+    messages = []
+
+    items = data.find_all('li', attrs={'class': 'grid__item'})
+    for item in items:
+        name = item.find_all('a', attrs={'class': 'full-unstyled-link'})[0].get_text().strip()
+        logger.info(f"confirming {name}")
+        if name.startswith('Keyball'):
+            status = "在庫あり"
+            if find := item.find('span', class_="badge badge--bottom-left color-inverse"):
+                status = find.get_text()
+
+            result = f"  {name}: {status}"
+            logger.info(result)
+            messages.append(result)
+
+    send_line_notify('\n' + '\n'.join(messages))
+
+
+def send_line_notify(notification_message):
+    line_notify_token = get_line_notify_token()
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token}'}
+    data = {'message': f'message: {notification_message}'}
+    requests.post(line_notify_api, headers=headers, data=data)
+
+
+def get_url():
+    return os.environ['URL']
+
+
+def get_line_notify_token():
+    return os.environ['LINE_NOTIFY_TOKEN']
